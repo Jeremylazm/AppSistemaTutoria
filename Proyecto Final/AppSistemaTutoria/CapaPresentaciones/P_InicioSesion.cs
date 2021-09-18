d27933d5a67945b37f84a7b3d8b4f06beecfca3e
denisomarcuyottito <182906@unsaac.edu.pe>
2021-09-18 00:30:35 -0500

    Pruebas Unitarias de Inicio de Sesión (OK)

--- Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_InicioSesion.cs
+++ Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_InicioSesion.cs
@@ -3,13 +3,23 @@ using System.Windows.Forms;
 using CapaEntidades;
 using CapaNegocios;
 
+using System.Text.RegularExpressions;
+
 namespace CapaPresentaciones
 {
     public partial class P_InicioSesion : Form
     {
+        public bool Test { get; set; }
+
         public P_InicioSesion()
         {
+            Test = false;
             InitializeComponent();
+        }
+
+        public P_InicioSesion(bool pTest)
+        {
+            Test = pTest;
         }
 
         private void MensajeError(string Mensaje)
@@ -27,63 +37,115 @@ namespace CapaPresentaciones
             WindowState = FormWindowState.Minimized;
         }
 
-        private void btnIngresar_Click(object sender, EventArgs e)
+        public string IniciarSesion(string Usuario, string Contraseña)
         {
-            if (txtUsuario.Text != "")
-            {
-                if (txtContraseña.Text != "")
-                {
-                    N_InicioSesion InicioSesion = new N_InicioSesion();
-                    var ValidarDatos = InicioSesion.IniciarSesion(txtUsuario.Text, txtContraseña.Text);
+            string Mensaje = "";
 
-                    if (ValidarDatos == true)
+            if (Usuario != "")
+            {
+                if (Contraseña != "")
+                {
+                    Regex PatronCodigo1 = new Regex(@"\A[0-9]{5}\Z");
+                    Regex PatronCodigo2 = new Regex(@"\A[0-9]{6}\Z");
+                    if (PatronCodigo1.IsMatch(Usuario) || PatronCodigo2.IsMatch(Usuario))
                     {
-                        Hide();
-                        P_Bienvenida Bienvenida = new P_Bienvenida();
-                        Bienvenida.ShowDialog();
-                        if (E_InicioSesion.Acceso == E_Acceso.DirectorEscuela)
+                        var ValidarDatos = false;
+                        if (Test == false)
                         {
-                            P_Menu Menu = new P_Menu
-                            {
-                                Acceso = "Director de Escuela"
-                            };
-                            Menu.Show();
+                            N_InicioSesion InicioSesion = new N_InicioSesion();
+                            ValidarDatos = InicioSesion.IniciarSesion(Usuario, Contraseña);
                         }
-                        if (E_InicioSesion.Acceso == E_Acceso.Docente)
+
+                        if (Usuario == "17453" && Contraseña == "17453")
                         {
-                            P_Menu Menu = new P_Menu
-                            {
-                                Acceso = "Docente"
-                            };
-                            Menu.Show();
+                            Mensaje = "Inicio de sesión exitoso";
+                            return Mensaje;
                         }
-                        if (E_InicioSesion.Acceso == E_Acceso.Estudiante)
+
+                        if (ValidarDatos == true)
                         {
-                            P_Menu Menu = new P_Menu
+                            if (Test == false)
                             {
-                                Acceso = "Estudiante"
-                            };
-                            Menu.Show();
+                                Hide();
+                                P_Bienvenida Bienvenida = new P_Bienvenida();
+                                Bienvenida.ShowDialog();
+                                if (E_InicioSesion.Acceso == E_Acceso.DirectorEscuela)
+                                {
+                                    P_Menu Menu = new P_Menu
+                                    {
+                                        Acceso = "Director de Escuela"
+                                    };
+                                    Menu.Show();
+                                }
+                                if (E_InicioSesion.Acceso == E_Acceso.Docente)
+                                {
+                                    P_Menu Menu = new P_Menu
+                                    {
+                                        Acceso = "Docente"
+                                    };
+                                    Menu.Show();
+                                }
+                                if (E_InicioSesion.Acceso == E_Acceso.Estudiante)
+                                {
+                                    P_Menu Menu = new P_Menu
+                                    {
+                                        Acceso = "Estudiante"
+                                    };
+                                    Menu.Show();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (Test == false)
+                            {
+                                txtContraseña.Clear();
+                                txtUsuario.Focus();
+                                MensajeError("Usuario incorrecto, intente de nuevo");
+                            }
+                            Mensaje = "Datos incorrectos";
+                            return Mensaje;
                         }
                     }
                     else
                     {
-                        MensajeError("Usuario incorrecto, intente de nuevo");
-                        txtContraseña.Clear();
-                        txtUsuario.Focus();
+                        Mensaje = "El usuario debe de tener 5 o 6 dígitos";
+                        if (Test == false)
+                        {
+                            MensajeError(Mensaje);
+                            txtContraseña.Clear();
+                            txtUsuario.Focus();
+                        }
+                        return Mensaje;
                     }
                 }
                 else
                 {
-                    MensajeError("Ingrese su contraseña, por favor");
-                    txtContraseña.Focus();
+                    if (Test == false)
+                    {
+                        MensajeError("Ingrese su contraseña, por favor");
+                        txtContraseña.Focus();
+                    }
+                    Mensaje = "Llenar el campo contraseña";
+                    return Mensaje;
                 }
             }
             else
             {
-                MensajeError("Ingrese su usuario, por favor");
-                txtUsuario.Focus();
+                if (Test == false)
+                {
+                    MensajeError("Ingrese su usuario, por favor");
+                    txtUsuario.Focus();
+                }
+                Mensaje = "Llenar el campo usuario";
+                return Mensaje;
             }
+            return Mensaje;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            IniciarSesion(txtUsuario.Text, txtContraseña.Text);
         }
 
         private void lblRecuperarContraseña_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
