37247e4da26f389c1f4abbeec86d3b1835d19120
Jeremylazm <72510370+Jeremylazm@users.noreply.github.com>
2021-09-20 21:17:33 -0500

    Expresiones Regulares en Editar Perfil Estudiante

--- Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_DatosEstudiante.cs
+++ Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_DatosEstudiante.cs
@@ -217,7 +217,11 @@ namespace CapaPresentaciones
                     try
                     {
                         DialogResult Opcion;
-                        Opcion = MessageBox.Show("¿Realmente desea editar el registro?", "Sistema de Tutoría", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                        if (Test == false)
+                            Opcion = MessageBox.Show("¿Realmente desea editar el registro?", "Sistema de Tutoría", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                        else
+                            Opcion = DialogResult.OK;
+
                         if (Opcion == DialogResult.OK)
                         {
                             byte[] PerfilArray = new byte[0];
