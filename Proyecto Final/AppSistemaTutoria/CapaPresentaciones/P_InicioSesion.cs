153efd353d57f82d8128b8c92afdd77479709ba1
denisomarcuyottito <182906@unsaac.edu.pe>
2021-09-21 07:43:03 -0500

    Comentarios P_InicioSesion

--- Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_InicioSesion.cs
+++ Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_InicioSesion.cs
@@ -9,10 +9,12 @@ namespace CapaPresentaciones
 {
     public partial class P_InicioSesion : Form
     {
+        // Atributo para pruebas
         public bool Test { get; set; }
 
         public P_InicioSesion()
         {
+            // Bandera de pruebas
             Test = false;
             InitializeComponent();
         }
@@ -22,6 +24,7 @@ namespace CapaPresentaciones
             Test = pTest;
         }
 
+        // Message Box para errores
         private void MensajeError(string Mensaje)
         {
             MessageBox.Show(Mensaje, "Sistema de Tutoría", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -37,8 +40,10 @@ namespace CapaPresentaciones
             WindowState = FormWindowState.Minimized;
         }
 
+        // Función parar iniciar sesión
         public string IniciarSesion(string Usuario, string Contraseña)
         {
+            // Mensaje cuando se están ejecutando las pruebas
             string Mensaje = "";
 
             if (Usuario == "" && Contraseña == "")
@@ -65,25 +70,34 @@ namespace CapaPresentaciones
                     if (PatronCodigo1.IsMatch(Usuario) || PatronCodigo2.IsMatch(Usuario) || PatronCodigo3.IsMatch(Usuario) || PatronCodigo4.IsMatch(Usuario))
                     {
                         var ValidarDatos = false;
+
+                        // Verificar el usuario y la contraseña
                         if (Test == false)
                         {
                             N_InicioSesion InicioSesion = new N_InicioSesion();
                             ValidarDatos = InicioSesion.IniciarSesion(Usuario, Contraseña);
                         }
 
+                        // Prueba de inicio de sesión exitoso
                         if (Usuario == "17453" && Contraseña == "17453" && Test == true)
                         {
                             Mensaje = "Inicio de sesión exitoso";
                             return Mensaje;
                         }
 
+                        // Si los datos son correctos
                         if (ValidarDatos == true)
                         {
+                            // Si no se está ejecuatando las pruebas
                             if (Test == false)
                             {
                                 Hide();
+
+                                //Mostrar mensaje de bienvenida
                                 P_Bienvenida Bienvenida = new P_Bienvenida();
+
                                 Bienvenida.ShowDialog();
+                                // Si el usuario es Director de Escuela
                                 if (E_InicioSesion.Acceso == E_Acceso.DirectorEscuela)
                                 {
                                     P_Menu Menu = new P_Menu
@@ -92,6 +106,8 @@ namespace CapaPresentaciones
                                     };
                                     Menu.Show();
                                 }
+
+                                // Si el usuario es Docente
                                 if (E_InicioSesion.Acceso == E_Acceso.Docente)
                                 {
                                     P_Menu Menu = new P_Menu
@@ -100,6 +116,8 @@ namespace CapaPresentaciones
                                     };
                                     Menu.Show();
                                 }
+
+                                // Si el usuario es Estudiante
                                 if (E_InicioSesion.Acceso == E_Acceso.Estudiante)
                                 {
                                     P_Menu Menu = new P_Menu
@@ -110,6 +128,7 @@ namespace CapaPresentaciones
                                 }
                             }
                         }
+                        // Si los datos son incorrectos
                         else
                         {
                             if (Test == false)
@@ -122,6 +141,7 @@ namespace CapaPresentaciones
                             return Mensaje;
                         }
                     }
+                    // Si la longitud del usuario no es de 5 o 6 dígitos
                     else
                     {
                         Mensaje = "El usuario debe de tener 5 o 6 dígitos";
@@ -134,6 +154,7 @@ namespace CapaPresentaciones
                         return Mensaje;
                     }
                 }
+                // Si el campo de la contraseña está vacío
                 else
                 {
                     if (Test == false)
@@ -145,6 +166,7 @@ namespace CapaPresentaciones
                     return Mensaje;
                 }
             }
+            // Si el campo del usuario está vacío
             else
             {
                 if (Test == false)
@@ -163,12 +185,14 @@ namespace CapaPresentaciones
             IniciarSesion(txtUsuario.Text, txtContraseña.Text);
         }
 
+        // Link label para recuperar la contraseña
         private void lblRecuperarContraseña_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             P_RecuperarContraseña RC = new P_RecuperarContraseña();
             RC.ShowDialog();
         }
 
+        // Enter para iniciar sesión cuando se ha digitado la contraseña
         private void txtContraseña_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == Convert.ToChar(Keys.Enter))
