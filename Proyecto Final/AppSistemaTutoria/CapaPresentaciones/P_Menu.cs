45f5f352eb0973fcd500340211817c1e7d2b57f3
Jeremylazm <72510370+Jeremylazm@users.noreply.github.com>
2021-09-22 05:39:36 -0500

    Revert "Imagenes 2.0"
    This reverts commit d6414e81864fe224f5b9120f495a7014f3e69a6c.

--- Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_Menu.cs
+++ Proyecto Final/AppSistemaTutoria/CapaPresentaciones/P_Menu.cs
@@ -46,14 +46,17 @@ namespace CapaPresentaciones
         {
             if (E_InicioSesion.Perfil == null)
             {
+                string fullImagePath = "";
                 if ((E_InicioSesion.Acceso == "Director de Escuela") || (E_InicioSesion.Acceso == "Administrador"))
-                    imgPerfil.Image = (Image)Properties.Resources.Perfil;
+                    fullImagePath = System.IO.Path.Combine(Application.StartupPath, @"../../Iconos/Perfil.png");
 
                 if (E_InicioSesion.Acceso == "Docente")
-                    imgPerfil.Image = (Image)Properties.Resources.Perfil_Docente;
+                    fullImagePath = System.IO.Path.Combine(Application.StartupPath, @"../../Iconos/Perfil Docente.png");
 
                 if (E_InicioSesion.Acceso == "Estudiante")
-                    imgPerfil.Image = (Image)Properties.Resources.Perfil_Estudiante;
+                    fullImagePath = System.IO.Path.Combine(Application.StartupPath, @"../../Iconos/Perfil Estudiante.png");
+
+                imgPerfil.Image = Image.FromFile(fullImagePath);
             }
             else
             {
